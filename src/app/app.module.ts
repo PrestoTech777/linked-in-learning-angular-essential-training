02fc10906a4e9023aab33739c35d3023b9e344ca
caleb <caleb.lopresto30@gmail.com>
2020-02-18 11:22:59 -0500

    02.08: Using other components in a component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MediaItemComponent } from './media-item/media-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MediaItemComponent
   ],
   imports: [
     BrowserModule,
